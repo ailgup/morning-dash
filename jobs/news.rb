0f3e5c044c1129f9851545586c7418a845828e8f
Chris Puglia <ailgup@mail.com>
2015-08-11 14:27:53 -0400

    comma

--- jobs/news.rb
+++ jobs/news.rb
@@ -5,7 +5,7 @@ require 'htmlentities'
 
 news_feeds = {
   "bbc-tech" => "http://feeds.bbci.co.uk/news/technology/rss.xml",
-  "sports" => "http://feeds.reuters.com/reuters/sportsNews"
+  "sports" => "http://feeds.reuters.com/reuters/sportsNews",
   "mashable" => "http://feeds.feedburner.com/Mashable",
   "techcrunch" => "http://feeds.feedburner.com/TechCrunch/",
   "opm" => "http://www.opm.gov/rss/operatingstatus.atom",
