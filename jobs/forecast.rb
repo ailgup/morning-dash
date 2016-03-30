6689ca72d4ffabd3ecc4f5f5f0e8b71b2d104d20
ailgup <ailgup@mail.com>
2016-03-30 07:56:53 -0400

    high temp

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -21,6 +21,7 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   response = http.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
   forecast = JSON.parse(response.body)  
   forecast_current_temp = forecast["currently"]["temperature"].round
+  forecast_high_temp = forecast["currently"]["temperatureMax"].round
   forecast_current_icon = forecast["currently"]["icon"]
   forecast_current_desc = forecast["currently"]["summary"]
   if forecast["minutely"]  # sometimes this is missing from the response.  I don't know why
@@ -33,5 +34,5 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   end
   forecast_later_desc   = forecast["hourly"]["summary"]
   forecast_later_icon   = forecast["hourly"]["icon"]
-  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
+  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", high_temp:: "#{forecast_high_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
 end
