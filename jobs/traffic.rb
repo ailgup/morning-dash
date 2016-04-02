c7229aee6ac2136d0dc71fa0e848a2a85fb0d6a8
ailgup <ailgup@mail.com>
2016-04-02 11:16:47 -0400

    added traffic locations

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -3,17 +3,21 @@ require 'net/https'
 require 'uri'
 require 'json'
 
-office_location = URI::encode('42.33716720443085,-71.08634632133482')
 key             = URI::encode('emfzpfv8dcee2dkkubqantzg')
 locations       = []
-locations << { name: "CyPhy", location: URI::encode('42.57545873043469,-70.97465425729752') }
+locations << { name: "CyPhy via Storrow", location: URI::encode('42.33716720443085,-71.08634632133482:circle(42.348449,-71.072096,100):42.57545873043469,-70.97465425729752') }
+locations << { name: "CyPhy via 93", location: URI::encode('42.33716720443085,-71.08634632133482:circle(42.335263,-71.06687665,100):42.57545873043469,-70.97465425729752') }
+locations << { name: "NEU via Rt.1", location: URI::encode('42.57545873043469,-70.97465425729752:circle(42.384754,-71.047544,100):42.33716720443085,-71.08634632133482') }
+locations << { name: "NEU via I-95", location: URI::encode('42.57545873043469,-70.97465425729752:circle(42.390849,-71.083114,100):42.33716720443085,-71.08634632133482') }
+locations << { name: "Townsend", location: URI::encode('42.33716720443085,-71.08634632133482:42.62207049,-71.67189181') }
+
 
 SCHEDULER.every '10m', :first_in => '15s' do |job|
     routes = []
 
     # pull data
     locations.each do |location|
-        uri = URI.parse("https://api.tomtom.com/routing/1/calculateRoute/#{office_location}:#{location[:location]}/json?routeType=fastest&traffic=true&travelMode=car&key=#{key}")
+        uri = URI.parse("https://api.tomtom.com/routing/1/calculateRoute/#{location[:location]}/json?routeType=fastest&traffic=true&travelMode=car&key=#{key}")
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
